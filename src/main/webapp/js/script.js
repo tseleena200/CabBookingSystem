edef04087e719d9f635384ff90b76b1e05db4c8d
Thenuri Seleena <tseleena200@gmail.com>
2025-01-29 21:15:00 +0530

    Did the login/registration front end

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -8,6 +8,21 @@ document.addEventListener("DOMContentLoaded", function () {
             window.location.href = "auth.html";
         }, 500);
     }, 3000);
+
+    document.getElementById("loginForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+
+        if (email === "admin@bookcab.com" && password === "123456") {
+            alert("Login successful! Redirecting...");
+            window.location.href = "dashboard.html";
+        } else {
+            alert("Invalid credentials. Please try again.");
+        }
+    });
+
 });
 
 
